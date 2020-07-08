f19ef974a51852eff2c296741738551c2a257665
Marcus Pallinger <git@marcus.pallinger.org>
2020-07-09 00:16:07 +1000

    Fix info

--- AusCovdUpdate/Program.cs
+++ AusCovdUpdate/Program.cs
@@ -4,6 +4,7 @@ namespace AusCovdUpdate
 {
     class Program
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage ("Style", "IDE0060:Remove unused parameter", Justification = "Main function")]
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
